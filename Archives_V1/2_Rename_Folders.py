bf248258d9b9b802e382d74964e4a96e66bbea01
SaturnX-Dev <SaturnX-Dev@outlook.com>
2023-11-15 13:52:31 -0600

    se corrigieron errores

--- Archives_V1/2_Rename_Folders.py
+++ Archives_V1/2_Rename_Folders.py
@@ -4,6 +4,7 @@ import shutil
 # Directorio de origen
 source_directory = r'Z:\Library\Reader'
 
+
 # Caracteres y símbolos a eliminar y reemplazar
 caracteres_a_eliminar = ["𓃠", "[烌]", "烌", "💮", "(", ")", "[", "]", "烏", "龙", "×͜×", "❬", "❭"]
 signos_a_eliminar = ["?", "¡", "¿", "!", "-", "_"]
@@ -11,6 +12,7 @@ acentos_a_reemplazar = {
     "á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u",
     "Á": "A", "É": "E", "Í": "I", "Ó": "O", "Ú": "U"
 }
+
 
 # Función para listar las carpetas
 def listar_carpetas(directorio):
@@ -20,67 +22,60 @@ def listar_carpetas(directorio):
 
 # Función para renombrar carpetas y fusionar carpetas con el mismo nombre
 def renombrar_y_fusionar_carpetas(directorio):
-    try:
-        carpetas_procesadas = {}
+    carpetas_procesadas = {}
 
-        def procesar_nombre(nombre):
-            # Eliminar caracteres y símbolos no deseados
-            for caracter in caracteres_a_eliminar:
-                nombre = nombre.replace(caracter, '')
-            for signo in signos_a_eliminar:
-                nombre = nombre.replace(signo, '')
-            for acento, reemplazo in acentos_a_reemplazar.items():
-                nombre = nombre.replace(acento, reemplazo)
+    def procesar_nombre(nombre):
+        # Eliminar caracteres y símbolos no deseados
+        for caracter in caracteres_a_eliminar:
+            nombre = nombre.replace(caracter, '')
+        for signo in signos_a_eliminar:
+            nombre = nombre.replace(signo, '')
+        for acento, reemplazo in acentos_a_reemplazar.items():
+            nombre = nombre.replace(acento, reemplazo)
 
-            # Eliminar guiones y números al principio y al final del nombre
-            nombre = nombre.lstrip('_')
-            while nombre and nombre[0].isdigit():
+        # Eliminar guiones y números al principio y al final del nombre
+        nombre = nombre.lstrip('_')
+        nombre = nombre.rstrip('_')
+        while nombre and (nombre[0].isdigit() or nombre[-1].isdigit()) and nombre[-2:] != '__':
+            if nombre[0].isdigit():
                 nombre = nombre[1:]
-
-            nombre = nombre.rstrip('_')
-            while nombre and nombre[-1].isdigit() and nombre[-2:] != '__':
+            elif nombre[-1].isdigit():
                 nombre = nombre[:-1]
 
-            # Capitalizar palabras
-            nombre = ' '.join(word.capitalize() for word in nombre.split())
+        # Capitalizar palabras
+        nombre = ' '.join(word.capitalize() for word in nombre.split())
 
-            return nombre.strip()
+        return nombre.strip()
 
-        for foldername in os.listdir(directorio):
-            if os.path.isdir(os.path.join(directorio, foldername)):
-                nuevo_nombre = procesar_nombre(foldername)
+    for foldername in os.listdir(directorio):
+        if os.path.isdir(os.path.join(directorio, foldername)):
+            nuevo_nombre = procesar_nombre(foldername)
 
-                if nuevo_nombre in carpetas_procesadas:
-                    ruta_existente = carpetas_procesadas[nuevo_nombre]
-                    ruta_actual = os.path.join(directorio, foldername)
-
-                    # Mover archivos a la carpeta existente
-                    for archivo in os.listdir(ruta_actual):
-                        shutil.move(os.path.join(ruta_actual, archivo), ruta_existente)
-
-                    # Eliminar la carpeta vacía
-                    os.rmdir(ruta_actual)
-                else:
-                    ruta_nueva = os.path.join(directorio, nuevo_nombre)
-                    # Renombrar solo si el nuevo nombre es diferente
-                    if ruta_nueva.lower() != os.path.join(directorio, foldername).lower():
-                        os.rename(os.path.join(directorio, foldername), ruta_nueva)
-                    carpetas_procesadas[nuevo_nombre] = ruta_nueva
-
-        logging.info("Carpetas renombradas y fusionadas.")
-
-    except Exception as e:
-        logging.error(f"Error durante el procesamiento: {e}")
-
+            if nuevo_nombre in carpetas_procesadas:
+                ruta_existente = carpetas_procesadas[nuevo_nombre]
+                ruta_actual = os.path.join(directorio, foldername)
+                
+                # Mover archivos a la carpeta existente
+                for archivo in os.listdir(ruta_actual):
+                    shutil.move(os.path.join(ruta_actual, archivo), ruta_existente)
+                
+                # Eliminar la carpeta vacía
+                os.rmdir(ruta_actual)
+            else:
+                ruta_nueva = os.path.join(directorio, nuevo_nombre)
+                # Renombrar solo si el nuevo nombre es diferente
+                if ruta_nueva.lower() != os.path.join(directorio, foldername).lower():
+                    os.rename(os.path.join(directorio, foldername), ruta_nueva)
+                carpetas_procesadas[nuevo_nombre] = ruta_nueva
+                
 # Menú de usuario
 while True:
     print("Menú:")
     print("1. Listar carpetas en la carpeta")
     print("2. Renombrar y fusionar carpetas")
-    print("3. Mostrar opciones del menú")
-    print("4. Salir")
+    print("3. Salir")
 
-    opcion = input("Selecciona una opción (1/2/3/4): ")
+    opcion = input("Selecciona una opción (1/2/3): ")
 
     if opcion == '1':
         print("Carpetas en la carpeta:")
@@ -88,14 +83,9 @@ while True:
     elif opcion == '2':
         print("Renombrando y fusionando carpetas en la carpeta...")
         renombrar_y_fusionar_carpetas(source_directory)
+        print("Carpetas renombradas y fusionadas.")
     elif opcion == '3':
-        print("Menú:")
-        print("1. Listar carpetas en la carpeta")
-        print("2. Renombrar y fusionar carpetas")
-        print("3. Mostrar opciones del menú")
-        print("4. Salir")
-    elif opcion == '4':
         print("Saliendo...")
         break
     else:
-        print("Opción no válida. Por favor, selecciona una opción válida (1/2/3/4).")
+        print("Opción no válida. Por favor, selecciona una opción válida (1/2/3).")
